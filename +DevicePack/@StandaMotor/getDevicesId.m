5d15e071291e5e7c497e6e03d3db303423db9128
Rasedujjaman <51413817+Rasedujjaman@users.noreply.github.com>
2021-08-14 20:31:50 +0200

    Delete +DevicePack directory

--- +DevicePack/@StandaMotor/getDevicesId.m
+++
@@ -1,9 +0,0 @@
-
-
-
-  function obj = getDevicesId(obj)
-            for ii = 1:obj.devices_count
-% %             obj.device_id(ii) = calllib('libximc','open_device', obj.device_names{ii});
-            disp(['ID of device [',num2str(ii),'] = ', num2str(obj.device_id(ii))]);
-            end
-  end
--- +DevicePack/@StandaMotor/goHome.m
+++
@@ -1,9 +0,0 @@
-    
-
-
-function obj = goHome(obj)
-     for ii = 1:obj.devices_count
-        calllib('libximc','command_move',  obj.device_id(ii), 0 ,0);   %% set the motor position at zero (home position)
-    end
-end
-        
